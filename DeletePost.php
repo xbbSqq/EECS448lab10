4c86868ee5332216200d98591509925b88fdb535
xbbSqq <78831071+xbbSqq@users.noreply.github.com>
2021-05-02 22:11:06 -0500

    Update DeletePost.php

--- DeletePost.php
+++ DeletePost.php
@@ -1,1 +1,19 @@
+<?php
+$post_list = $_POST["postForm"];
+$mysqli = new mysqli("mysql.eecs.ku.edu", "mefelsen", "evog9ooY", "mefelsen");
 
+if(!empty($post_list)) {
+  $j = count($post_list);
+  echo "deleted " . $j . " post(s)<br>";
+  for($i = 0; $i < $N; $i++)
+  {
+    $query = "DELETE FROM posts WHERE post_id = '" . $post_list[$i] . "'";
+    if ($result = $mysqli->query($query)) {
+      echo $post_list[$i] . "<br>";
+    } else {
+      echo "Error deleting post:  " . $post_list[$i] . "<br>";
+    }
+  }
+}
+$mysqli->close();
+?>
